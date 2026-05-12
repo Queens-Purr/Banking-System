d53cd58dc8c6650a579c044113ae05c6957f3b21
marreahsamantha <lusdocmarreahsamantha@gmail.com>
2026-05-12 16:54:18 +0000

    Validation and Error Handling

--- BankingSystem.py
+++ BankingSystem.py
@@ -4,28 +4,50 @@ balance = 1000
 # Transaction history list
 transactions = []
 
+# Input Valdation
+def validate_amount(amount):
+   if amount <= 0:
+      print("Invalid amount. Amount must be greater than 0.")
+      return False
+
+   return True
+
 # Deposit money function
 def deposit(amount):
     global balance
-
-    if amount > 0:
+    
+    if validate_amount(amount):
+#User Confirmation
+      confirm = input(f"Confirm deposit of ₱{amount}? (yes/no): ")
+    if confirm.lower() == "yes":
         balance += amount
         transactions.append(f"Deposited: {amount}")
         print(f"Successfully deposited {amount}")
+        print("New Balance: ₱{balance}")
     else:
-        print("Invalid deposit amount")
+        print("Deposit Cancelled!")
 
 # Withdraw money function
 def withdraw(amount):
     global balance
 
-    if amount > 0 and amount <= balance:
-        balance -= amount
+    if validate_amount(amount):
+        if amount > 0 and amount <= balance:
+            balance -= amount
         transactions.append(f"Withdrawn: {amount}")
         print(f"Successfully withdrew {amount}")
     else:
-        print("Insufficient balance or invalid amount")
+        print(" Warning:Insufficient balance or invalid amount")
+# User Confirmation
+        confirm = input(f"Confirm withdrawal of ₱{amount}? (yes/no): ")
+    if confirm.lower() == "yes":
+        balance -= amount
+        transactions.append(f"Withdrawn: ₱{amount}")
+        print(f"Successfully withdrew ₱{amount}")
+        print(f"Remaining balance: ₱{balance}")
 
+    else:
+        print("Withdrawal cancelled.")
 # Check balance function
 def check_balance():
     print(f"Current balance: {balance}")
